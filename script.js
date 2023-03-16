6db4edbee1bcb5358099f8da0ba715b620071abf
Alyoshka <alyoshkamyshko@gmail.com>
2023-03-15 21:06:50 -0700

    Create Gameboard factory function with placeShip method

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function Ship(length) {
+function Ship(length, coords) {
     function hit() {
         this.timesHit++;
     }
@@ -7,8 +7,46 @@ function Ship(length) {
         return this.timesHit >= this.length;
     }
 
-    return { length, timesHit: 0, hit, isSunk }
+    return { length, coords, timesHit: 0, hit, isSunk }
 }
 
+function Gameboard() {
+    const ships = [];
 
-export { Ship }
+    function placeShip(shipLength, row, col, rotation) {
+        if ((rotation == 'vertical' && row + shipLength > 10) || (rotation == 'horizontal' && col + shipLength > 10)) return;
+        const coords = generateShipCoords(this.ships, shipLength, row, col, rotation);
+        if (coords === undefined) return;
+        const ship = Ship(shipLength, coords);
+        this.ships.push(ship);
+    }
+
+    function generateShipCoords(ships, shipLength, row, col, rotation) {
+        let coords = [];
+        if (rotation == 'vertical') {
+            for (let i = 0; i < shipLength; i++) {
+                if (!validateCoord(ships, row + i, col)) return;
+                coords.push([row + i, col]);
+            }
+        } else {
+            for (let i = 0; i < shipLength; i++) {
+                if (!validateCoord(ships, row, col + i)) return;
+                coords.push([row, col + i]);
+            }
+        }
+        return coords;
+    }
+
+    function validateCoord(ships, row, col) {
+        return ships.find(ship => {
+            console.log(ship);
+            return ship.coords.some(coord => {
+                return coord[0] == row && coord[1] == col;
+            });
+        }) == undefined;
+    }
+
+    return { ships, placeShip }
+}
+
+export { Ship, Gameboard }
